f8e13e3d0c3966892e89a93604f4872a10a02cd6
aloe-droid <csjune99@naver.com>
2026-02-14 23:51:21 +0900

    feat: add NavigationBar Color

--- app/src/main/java/com/aloe_droid/ongi/ui/OnGiApp.kt
+++ app/src/main/java/com/aloe_droid/ongi/ui/OnGiApp.kt
@@ -1,9 +1,11 @@
 package com.aloe_droid.ongi.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -35,6 +37,7 @@ fun OnGiApp(navController: NavHostController = rememberNavController()) {
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.surfaceContainer)
             .navigationBarsPadding(),
         snackbarHost = {
             SnackbarHost(hostState = snackBarHostState)
